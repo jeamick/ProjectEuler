76063ffab88503ff8f38ff2d28dfcd0e8e4f5c6a
jeamick <nounahon.j@gmail.com>
2018-12-25 20:29:20 +0100

    Update Python/Problem1.py

--- Python/Problem1.py
+++ Python/Problem1.py
@@ -10,10 +10,10 @@ def RefactorIterator(a, b):
 
     return int(a/b) if (a%b==0) else int(a/b) + 1
     
-def SumDivInfTo1000(Nmbre):
+def SumDivInfToNmbre(Nmbre):
       
     return 3 * sum(range(1, RefactorIterator(Nmbre, 3))) + 5 * sum(range(1, RefactorIterator(Nmbre, 5))) - 15 * sum(range(1, RefactorIterator(Nmbre, 15)))
         
 if __name__ == "__main__":
 
-    print(SumDivInfTo1000(1000))
+    print(SumDivInfToNmbre(1000))
