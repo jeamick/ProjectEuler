08e3a7c91ebde91e02afe89e8af09f5f46b69754
jeamick <nounahon.j@gmail.com>
2018-12-25 20:24:47 +0100

    update Variables
    Visibility

--- Python/Problem1.py
+++ Python/Problem1.py
@@ -6,7 +6,7 @@
 # https://github.com/jeamick/ProjectEuler
 
 
-#Maybe Better Solution 
+# Maybe Better Solution 
 def RefactorIterator(a, b):
 
     if(a%b==0):
@@ -16,9 +16,9 @@ def RefactorIterator(a, b):
 
     return p
     
-def SumDivInfTo1000(a):
+def SumDivInfTo1000(Nmbre):
       
-    return 3 * sum(range(1, RefactorIterator(a, 3))) + 5 * sum(range(1, RefactorIterator(a, 5))) - 15 * sum(range(1, RefactorIterator(a, 15)))
+    return 3 * sum(range(1, RefactorIterator(Nmbre, 3))) + 5 * sum(range(1, RefactorIterator(Nmbre, 5))) - 15 * sum(range(1, RefactorIterator(Nmbre, 15)))
         
 if __name__ == "__main__":
 
