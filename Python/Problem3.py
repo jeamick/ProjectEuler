80300cc3a8842ca131fe1764fdd8d6b8bea91d9c
jeamick <nounahon.j@gmail.com>
2018-12-26 21:12:16 +0100

    Math Import

--- Python/Problem3.py
+++ Python/Problem3.py
@@ -2,3 +2,9 @@
 # Copyright (c) jeamick. All rights reserved.
 # https://projecteuler.net/problem=3
 # https://github.com/jeamick/ProjectEuler
+
+
+if __name__ == "__main__":
+    import math
+    
+    pass
