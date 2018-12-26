546a60e350b031a621e04b5db63ad741a1952202
jeamick <nounahon.j@gmail.com>
2018-12-26 21:23:12 +0100

    Update Problem 3
    Solution Euler problem 3

--- Python/Problem3.py
+++ Python/Problem3.py
@@ -3,8 +3,24 @@
 # https://projecteuler.net/problem=3
 # https://github.com/jeamick/ProjectEuler
 
+def primeFactors(n): 
+    ListOfPrimeFactor = [] 
+    while n % 2 == 0: 
+        ListOfPrimeFactor.append(2), 
+        n = n / 2
+          
+    for i in range(3, int(math.sqrt(n))+1, 2): 
+          
+        while n % i == 0: 
+            ListOfPrimeFactor.append(int(i)), 
+            n = n / i 
+              
+    if n > 2: 
+        ListOfPrimeFactor.append(int(n))
+    
+    return ListOfPrimeFactor
 
 if __name__ == "__main__":
     import math
+    print(primeFactors(600851475143)[len(primeFactors(454822))-1])
     
-    pass
