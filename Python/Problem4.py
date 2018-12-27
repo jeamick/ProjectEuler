25f2554cc90c933ba32348e5e34fe5ce6ec96471
jeamick <nounahon.j@gmail.com>
2018-12-27 21:04:48 +0100

    Create Problem 4
    Start Problem4

---
+++ Python/Problem4.py
@@ -0,0 +1,9 @@
+# Solution to Project Euler problem 4
+# Copyright (c) jeamick. All rights reserved.
+# https://projecteuler.net/problem=4
+# https://github.com/jeamick/ProjectEuler
+
+
+
+if __name__ == "__main__":
+    pass
