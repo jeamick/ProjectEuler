4d7c72312f3e015176291f977b8f5fad85c7759b
JeanMick <nounahon.j@gmail.com>
2019-01-19 01:29:05 +0100

    Problem4 update

--- Python/Problem4.py
+++ Python/Problem4.py
@@ -3,7 +3,16 @@
 # https://projecteuler.net/problem=4
 # https://github.com/jeamick/ProjectEuler
 
+def Palindrome_Number():
 
+    palind = None
+    for n in xrange(100, 1000):
+        for m in xrange(100, 1000):
+            prod = n * m
+            if str(prod) == str(prod)[::-1]:
+                if palind is None or prod > palind:
+                    palind = prod
+    return palind
 
 if __name__ == "__main__":
-    pass
+    print(Palindrome_Number())
