909ec0023ae3acf20416f8f7165a1506a7cd98ca
jeamick <nounahon.j@gmail.com>
2019-01-19 03:59:52 +0100

    Faster version
    0.04 secs...

--- Python/Problem7.py
+++ Python/Problem7.py
@@ -10,25 +10,27 @@ def timer(func):
         print('Time elapsed: ', after - before)
         return rv
     return f
+        
+
+ def fast_nth_prime(n, limit=125000):
     
-def isPrime(n): 
-    if (n <= 1): 
-        return False
-    
-    for i in range(2, n): 
-        if (n % i == 0): 
-            return False
-    return True
-    
+    if limit % 2 != 0: 
+        limit += 1
+    primes = [True] * limit
+    primes[0], primes[1] = [None] * 2
+    count = 0 # how many primes have we found?
+    for ind, val in enumerate(primes):
+        if val is True:
+            # sieve out non-primes by multiples of known primes
+            primes[ind*2::ind] = [False] * (((limit - 1)//ind) - 1)
+            count += 1
+        if count == n: 
+            return ind
+    return False
+
 @timer
 def test():
-    Tabu = []
-    i = 0
-    while(len(Tabu)<10001):
-        i = i + 1
-        if(isPrime(i) is True):
-            Tabu.append(i)
-    print i
+    print(fast_nth_prime(10001))
 
 if __name__ == '__main__':
     test()
