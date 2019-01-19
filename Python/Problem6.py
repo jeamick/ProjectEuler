979052e9cd1e038e52663dc31fcd9bbd629b85c4
jeamick <nounahon.j@gmail.com>
2019-01-19 03:31:21 +0100

    Solution

--- Python/Problem6.py
+++ Python/Problem6.py
@@ -3,5 +3,11 @@
 # https://projecteuler.net/problem=6
 # https://github.com/jeamick/ProjectEuler
   
+def FunctionNcarre(n):
+  '''
+  Somme of n*n integers
+  '''
+  return (n*(n+1)*(2*n+1)/6)
+  
 if __name__ == '__main__':
-  pass
+  print FunctionNcarre(100)*FunctionNcarre(100)-FunctionNcarre(100)
