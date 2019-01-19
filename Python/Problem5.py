741af3530256cd02f1e46c47b8d0936d2df1a93d
jeamick <nounahon.j@gmail.com>
2019-01-19 03:16:50 +0100

    Final Solution

--- Python/Problem5.py
+++ Python/Problem5.py
@@ -1,4 +1,7 @@
-
+# Solution to Project Euler problem 5
+# Copyright (c) jeamick. All rights reserved.
+# https://projecteuler.net/problem=5
+# https://github.com/jeamick/ProjectEuler
 
 def gcd(a, b):
   while b > 0:
@@ -9,5 +12,9 @@ def lcm(a, b):
   return (a * b) / gcd(a, b)
   
 if __name__ == '__main__':
-  pass
+  
+  llcm = lcm(11, 12)
+  for n in range(12, 20):
+    llcm = lcm(n, llcm)
+  print llcm
  
