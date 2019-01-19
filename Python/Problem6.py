25196e8711d0b539e908da37405558e5d5539451
jeamick <nounahon.j@gmail.com>
2019-01-19 03:29:29 +0100

    Initialisation problem 6

---
+++ Python/Problem6.py
@@ -0,0 +1,7 @@
+# Solution to Project Euler problem 6
+# Copyright (c) jeamick. All rights reserved.
+# https://projecteuler.net/problem=6
+# https://github.com/jeamick/ProjectEuler
+  
+if __name__ == '__main__':
+  pass
