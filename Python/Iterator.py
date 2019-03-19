63e443b57b0da564640fe1634e990c0cda770214
jeamick <nounahon.j@gmail.com>
2019-03-19 21:25:52 +0100

    Update Iterator.py

--- Python/Iterator.py
+++ Python/Iterator.py
@@ -7,11 +7,11 @@ class Counter:
         # for "initialize"
         self.current = low
         self.high = high
-
+    
     def __iter__(self):
         # first magic method to make this object iterable
         return self
-    
+   
     def __next__(self):
         # second magic method
         if self.current > self.high:
