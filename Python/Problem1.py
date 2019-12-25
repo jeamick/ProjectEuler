6ec06cfa72b35f704dec8a714a8e690d689f4e37
jeamick <nounahon.j@gmail.com>
2019-12-25 21:06:30 +0100

    update int

--- Python/Problem1.py
+++ Python/Problem1.py
@@ -5,10 +5,10 @@
 
 
 # Maybe Better/Efficient Solution 
-def RefactorIterator(a, b):
+def RefactorIterator(a :int, b: int):
     return int(a/b) if (a%b==0) else int(a/b) + 1
     
-def SumDivInfToNmbre(Nmbre): 
+def SumDivInfToNmbre(Nmbre :int): 
     return 3 * sum(range(1, RefactorIterator(Nmbre, 3))) + 5 * sum(range(1, RefactorIterator(Nmbre, 5))) - 15 * sum(range(1, RefactorIterator(Nmbre, 15)))
         
 if __name__ == "__main__":
